3af48a8a9afaa3035707c641391a1a2fd401ffd6
Prashasthi Ayuchchada Parakrama <khprashasthi@gmail.com>
2025-07-21 22:43:37 +0530

    error fixed 🛠

--- src/components/menu-card.jsx
+++ src/components/menu-card.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default  function MenuCard(props) {
   return (
-    <div className='w-[100px] h-[100px] rounded-md m-1 text-sm hover:bg-gray-100 m-2 text-2xl flex justify-center items-center   '            >
+    <div className='w-[100px] h-[100px] rounded-md m-1  hover:bg-gray-100  text-2xl flex justify-center items-center   '            >
        <span> {props.name}  </span>
      
     </div>
